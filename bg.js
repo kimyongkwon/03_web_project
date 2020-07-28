70209e0dd4c4caf72bf11a9163979e7fed1d81c6
KimYongKwon <dydrnjs518@gmail.com>
2020-07-28 16:15:48 +0900

    background, weather part
    - random background setting
    - weather, geolocation setting

---
+++ bg.js
@@ -0,0 +1,27 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 3;
+
+// function handleImage() {
+//   console.log("image load");
+// }
+
+function paintImage(imgNumber) {
+  const image = new Image();
+  image.src = `/images/${imgNumber + 1}.jpg`;
+  image.classList.add("bgImage");
+  body.appendChild(image);
+
+  //   image.addEventListener("loadend", handleImage);
+}
+function genRandom() {
+  const number = Math.floor(Math.random() * IMG_NUMBER);
+  return number;
+}
+
+function init() {
+  const random = genRandom();
+  paintImage(random);
+}
+
+init();
---
+++ weather.js
@@ -0,0 +1,58 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "acfc8d8bc8803e37ba1ab88a0bdd15da";
+const COORDS = "coords";
+
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metirc`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature} @ ${place}`;
+    });
+}
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+  console.log("can't");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoord() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    const parseCoords = JSON.parse(loadedCoords);
+    getWeather(parseCoords.latitude, parseCoords.longitude);
+    console.log(parseCoords);
+  }
+}
+
+function init() {
+  loadCoord();
+}
+
+init();
